71ae04d1539dd8b88577871c0427715eba269d98
Ting Yu <ting.yu@auckland.ac.nz>
2009-03-26 22:40:52 +0000

    change the testscript using python
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/branches/developers/tingy@438 61e36070-c886-4827-b7c0-ed9e4dc59cc8

---
+++ AnalyticLaplace/testscript.py
@@ -0,0 +1,28 @@
+import hashlib
+import sys
+import commands
+
+def md5sum(filename):
+  myfile = open(filename)
+  m = hashlib.md5()
+  
+  for aline in myfile :
+    m.update(aline)
+  myfile.close()
+  return m.hexdigest()
+
+if len(sys.argv)!=2 :
+  print "Invalid number of arguments"
+  sys.exit(1)
+name = sys.argv[1]
+print commands.getoutput(name)
+
+actualfile = "LaplaceExample"
+expectedfile = "ExpectedLaplaceExample"
+md5actual =  md5sum(actualfile)
+md5expected =  md5sum(expectedfile)
+if md5actual == md5expected :
+  print "Analytic Laplace Example Testcase3 - Output file check is successfully completed."
+else :
+  print "ERROR:The output file is not as expected!"
+  sys.exit(1)
