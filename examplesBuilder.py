baf2ac2a95fb50bb84e07dd33692c2c348f5ca8f
Ting Yu <ting.yu@auckland.ac.nz>
2009-07-06 03:01:47 +0000

    refactor and separate build script and test script. See tracker item 1768
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@580 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- examplesBuilder.py
+++ examplesBuilder.py
@@ -3,53 +3,41 @@ import os, sys
 cwd = os.getcwd();
 success = 1;
 logDir = cwd + "/../../build/logs";
-rootUrl = "http://autotest.bioeng.auckland.ac.nz:8888/"
+rootUrl = "https://autotest.bioeng.auckland.ac.nz/opencmiss-admin/"
 
-if os.path.isdir(logDir):
-  os.system("rm -r " + logDir)
-  os.rmdir(cwd + "/../../build")
-os.mkdir(cwd + "/../../build")
-os.mkdir(logDir);
+if not os.path.isdir(logDir):
+  os.mkdir(cwd + "/../../build")
+  os.mkdir(logDir);
 compiler = sys.argv[1];
 
+
 def buildExample(path) :
-  global success,compiler,logDir;
-  os.chdir(path)
-  err=0
-  if compiler == 'gnu' :
-    err=os.system("make COMPILER=gnu > " + logDir + "/build-" + path.replace('/', '_')+ " 2>&1")
-  elif compiler == 'intel' :
-    err=os.system("make > " + logDir + "/" + path.replace('/', '_') + " 2>&1")
-  if err==0 :
-    print "Building " + path.replace('/', '_')+': success'
-  else :
-    success=0
-    print "Building " + path.replace('/', '_')+': fail'
-  os.chdir(cwd)
-  return;
-  
-def testExample(path, nodes) :
-  global success,compiler,logDir;
-  os.putenv('PATH', os.environ['PATH']+':'+cwd+'/../../../opencmissextras/cm/external/x86_64-linux-debug-'+compiler+'/bin')
-  os.chdir(path)
-  err=os.system('mpiexec -n '+nodes+' bin/x86_64-linux/'+path.rpartition('/')[2]+'Example-debug-'+compiler+' 10 10 0 '+nodes+" > " + logDir + "/test-" + path.replace('/', '_')+ " 2>&1")
-  if err==0 :
-    print "Testing" + path.replace('/', '_')+': success'
-  else :
-    success=0
-    print "Testing" + path.replace('/', '_')+': fail'
-  os.chdir(cwd)
-  return;
+   global success,compiler,logDir;
+   newDir = logDir
+   for folder in path.split('/') :
+     newDir = newDir + '/' + folder
+     if not os.path.isdir(newDir):
+       os.mkdir(newDir)
+   os.chdir(path)
+   if os.path.exists(newDir + "/build-" + compiler) :
+     os.remove(newDir + "/build-" + compiler)
+   err=os.system("make COMPILER=" + compiler + " > " + newDir + "/build-" + compiler +" 2>&1")
+   if err==0 :
+     print "Building " + path+': success'
+   else :
+     success=0
+     print "Building " + path+': fail'
+   os.chdir(cwd)
+   return;
 
 buildExample("ClassicalField/AnalyticLaplace")
 buildExample("ClassicalField/Diffusion")
 buildExample("ClassicalField/Helmholtz")
 buildExample("ClassicalField/Laplace")
-testExample("ClassicalField/Laplace", '2')
 buildExample("ClassicalField/NonlinearPoisson")
 
 buildExample("Bioelectrics/Monodomain")
-
+  
 buildExample("FluidMechanics/StokesFlow/HexChannel")
 buildExample("FluidMechanics/StokesFlow/HexPipe")
 buildExample("FluidMechanics/StokesFlow/SingleElement")
---
+++ examplesTester.py
@@ -0,0 +1,45 @@
+import os, sys
+
+cwd = os.getcwd();
+success = 1;
+logDir = cwd + "/../../build/logs";
+rootUrl = "https://autotest.bioeng.auckland.ac.nz/opencmiss-admin/"
+
+if not os.path.isdir(logDir):
+  os.mkdir(cwd + "/../../build")
+  os.mkdir(logDir);
+compiler = sys.argv[1];
+os.system('mpd.py &')
+
+def testExample(id, path, nodes) :
+   global success,compiler,logDir;
+   os.putenv('PATH', os.environ['PATH']+':'+cwd+'/../../../opencmissextras/cm/external/x86_64-linux-debug-'+compiler+'/bin')
+   newDir = logDir
+   for folder in path.split('/') :
+     newDir = newDir + '/' + folder
+     if not os.path.isdir(newDir):
+       os.mkdir(newDir)
+   os.chdir(path)
+   if os.path.exists(newDir + "/test"+id+"-" + compiler) :
+     os.remove(newDir + "/test"+id+"-" + compiler)
+   execPath='bin/x86_64-linux/'+path.rpartition('/')[2]+'Example-debug-'+compiler
+   err=os.system('mpiexec.py -n ' + nodes + " " + execPath + ' 10 10 0 '+nodes+" > " + newDir + "/test" + id + "-" + compiler + " 2>&1")
+   if not os.path.exists(execPath) :
+     err=-1
+   if err==0 :
+     print "Testing " + path + id + ': success'
+   else :
+     success=0
+     print "Testing" + path + id + ': fail'
+   os.chdir(cwd)
+   return;
+
+
+testExample('1', "ClassicalField/Laplace", '1') 
+#testExample('2', "ClassicalField/Laplace", '2')
+
+print "See %slogs_x86_64-linux-%s for detail" % (rootUrl, compiler) 
+if success==0 :
+  raise RuntimeError
+
+
