ddc855d32897c39b8a1e5b43a1c83d4fef4da4a9
Ting Yu <ting.yu@auckland.ac.nz>
2010-03-31 04:29:48 +0000

    argument fix for ndiff.
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1003 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- examplesTester.py
+++ examplesTester.py
@@ -99,7 +99,7 @@ testExample(id='1',path="FluidMechanics/NavierStokes/Dynamic",nodes='1',input='\
 
 
 testExample(id='1',path="FluidMechanics/Darcy/Static",nodes='1',input='\n')
-testExample(id='1',path="FluidMechanics/Darcy/QuasistaticMaterial",nodes='1',input='\n',expectedFile="expected_results/TIME_STEP_0003.exnode",actualFile="output/TIME_STEP_0003.exnode")
+testExample(id='1',path="FluidMechanics/Darcy/QuasistaticMaterial",nodes='1',input='\n',ndiffDir="expected_results",outputDir="output")
 #testExample(id='1',path="FluidMechanics/Darcy/VenousCompartment",nodes='1',input='4\n4\n0\n1')
 
 #testExample(id='1',path="FiniteElasticity/UniAxialExtension",nodes='1',input='4\n4\n0\n1')
