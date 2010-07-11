8edcd0fbf8741ef54377554628771db0ca7b72a5
Ali Pashaei <ali.pashaei@upf.edu>
2010-07-11 16:06:40 +0000

    to commit two examples to check if the Hamilton-Jacobi routine works. The examples are including:
    1) TwoElmnTetExample: is designed to check the function of FMM solver. It consist of a domain consisting two tetrahedral elements. A single seed is located at node number 1.
    
    2) GeodesicEx1Example: is designed to check the function of Geodesic solver. The domain in this example is a cubic volume consisting 39035 tetrahedral elements. The solution to this example gives the geodesic between Node_1=1 and Node_2=491.
    
    
    
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1339 61e36070-c886-4827-b7c0-ed9e4dc59cc8

---
+++ ClassicalField/Hamilton_Jacobi/GeodesicEx1/src/GeodesicEx1Example.f90
@@ -0,0 +1,208 @@
+!> \file
+!> $Id: GeodesicEx1Example.f90 20 2007-05-28 20:22:52Z cpb $
+!> \author Ali PASHAEI
+!> \brief This is an example program to find the geodesic using openCMISS calls.
+!>
+!> \section LICENSE
+!>
+!> Version: MPL 1.1/GPL 2.0/LGPL 2.1
+!>
+!> The contents of this file are subject to the Mozilla Public License
+!> Version 1.1 (the "License"); you may not use this file except in
+!> compliance with the License. You may obtain a copy of the License at
+!> http://www.mozilla.org/MPL/
+!>
+!> Software distributed under the License is distributed on an "AS IS"
+!> basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+!> License for the specific language governing rights and limitations
+!> under the License.
+!>
+!> The Original Code is openCMISS
+!>
+!> The Initial Developer of the Original Code is  University of Pompeu Fabra,
+!> Barcelona, Spain, University of Auckland, Auckland, New Zealand and 
+!> University of Oxford, Oxford, United Kingdom. Portions created by the 
+!> University of Auckland and University of Oxford are Copyright (C) 2007 
+!> by the University of Pompeu Fabra, the University of Auckland and
+!> the University of Oxford. All Rights Reserved.
+!>
+!> Contributor(s):
+!>
+!> Alternatively, the contents of this file may be used under the terms of
+!> either the GNU General Public License Version 2 or later (the "GPL"), or
+!> the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+!> in which case the provisions of the GPL or the LGPL are applicable instead
+!> of those above. If you wish to allow use of your version of this file only
+!> under the terms of either the GPL or the LGPL, and not to allow others to
+!> use your version of this file under the terms of the MPL, indicate your
+!> decision by deleting the provisions above and replace them with the notice
+!> and other provisions required by the GPL or the LGPL. If you do not delete
+!> the provisions above, a recipient may use your version of this file under
+!> the terms of any one of the MPL, the GPL or the LGPL.
+!>
+
+!> \example ClassicalField/Hamilton_Jacobi/GeodesicEx1/src/GeodesicEx1Example.f90
+!! Example program to solve a Laplace equation using OpenCMISS calls.
+!! \htmlinclude ClassicalField/Laplace/Laplace/history.html
+!!
+!<
+
+PROGRAM GeodesicEx1
+!...................................................................................
+
+! OpenCMISS Modules
+
+  USE OPENCMISS
+
+#ifdef WIN32
+  USE IFQWIN
+#endif
+
+!...................................................................................
+  IMPLICIT NONE
+!...................................................................................
+
+  !Program variables
+
+  INTEGER(CMISSIntg), PARAMETER :: CoordinateSystemUserNumber=1
+  INTEGER(CMISSIntg), PARAMETER :: RegionUserNumber=2
+
+  !local variables
+
+  INTEGER(CMISSIntg) :: I,J,SEED_POINT,TOTAL_NUMBER_OF_CONNECTIVITY
+  INTEGER(CMISSIntg) :: NUMBER_OF_NODES_PER_ELEMENT,TOTAL_NUMBER_OF_NODES,TOTAL_NUMBER_OF_ELEMENTS
+  INTEGER(CMISSIntg) :: NODE_1,NODE_2
+  REAL(CMISSDP) :: SPEED_FUNCTION_ALONG_EIGEN_VECTOR(3)
+  CHARACTER (LEN=300) :: INPUT_FILE_NAME
+  CHARACTER (LEN=300) :: OUTPUT_FILE_NAME
+  CHARACTER (LEN=300) :: OUTPUT_FILE_FIELD_TITLE
+  CHARACTER (LEN=10)  :: INPUT_FILE_FORMAT
+  CHARACTER (LEN=10)  :: OUTPUT_FILE_FORMAT
+  CHARACTER (LEN=12)  :: MATERIAL_BEHAVIOUR
+  CHARACTER (LEN=10)  :: INPUT_TYPE_FOR_SPEED_FUNCTION
+  CHARACTER (LEN=10)  :: INPUT_TYPE_FOR_SEED_VALUE
+  CHARACTER (LEN=10)  :: INPUT_TYPE_FOR_CONDUCTIVITY
+
+  INTEGER(CMISSIntg) :: Err
+
+  TYPE(CMISSCoordinateSystemType) :: CoordinateSystem,WorldCoordinateSystem
+  TYPE(CMISSRegionType) :: WorldRegion
+  
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: NODE_LIST
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: SPEED_FUNCTION_TABLE
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: CONDUCTIVITY_TENSOR
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: SPEED_FUNCTION_TABLE_ON_CONNECTIVITY
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: CONDUCTIVITY_TENSOR_ON_CONNECTIVITY
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: SEED_VALUE
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: SNAPSHOT_SEED_VALUE
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: RELATIVE_ERROR
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: SEED_VALUE_EXACT_SOLUTION
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: TRACE_NODES
+  
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:,:):: CONNECTIVITY_LIST
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:,:):: ELEMENT_LIST
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: CONNECTIVITY_NUMBER
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: TRACE_NODE
+
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: COLUMN_INDEX
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: RAW_INDEX
+  CHARACTER (LEN=10), ALLOCATABLE, DIMENSION(:)  :: STATUS_MASK
+
+#ifdef WIN32
+  !Initialise QuickWin
+  QUICKWIN_WINDOW_CONFIG%TITLE="General Output" !Window title
+  QUICKWIN_WINDOW_CONFIG%NUMTEXTROWS=-1 !Max possible number of rows
+  QUICKWIN_WINDOW_CONFIG%MODE=QWIN$SCROLLDOWN
+  QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
+  IF(.NOT.QUICKWIN_STATUS) QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
+#endif
+
+  !Intialise OpenCMISS
+  CALL CMISSInitialise(WorldCoordinateSystem,WorldRegion,Err)
+  CALL CMISSCoordinateSystemTypeInitialise(CoordinateSystem,Err)
+  CALL CMISSCoordinateSystemCreateStart(CoordinateSystemUserNumber,CoordinateSystem,Err)
+  CALL CMISSCoordinateSystemDimensionSet(CoordinateSystem,3,Err)
+  CALL CMISSCoordinateSystemCreateFinish(CoordinateSystem,Err)
+
+! INPUT_FILE_FORMAT :   "TABC"   -> tabc format
+!			"TETGEN" -> tetgen format
+  INPUT_FILE_FORMAT  = "TETGEN"
+
+! INPUT_FILE_NAME :   in order to calculate TOTAL_NUMBER_OF_NODES
+  INPUT_FILE_NAME = "src/GeodesicEx1"
+
+! Geodesic points :   Node_1 and Node_2 are Node Number of two ends of the geodesic path in the domain.
+  NODE_1=451
+  NODE_2=1
+
+! TOTAL_NUMBER_OF_NODES : to set input parameters
+  CALL CMISS_NUMBER_OF_INPUT_NODES(INPUT_FILE_NAME,INPUT_FILE_FORMAT,TOTAL_NUMBER_OF_NODES,TOTAL_NUMBER_OF_ELEMENTS,&
+  & TOTAL_NUMBER_OF_CONNECTIVITY,Err)
+!  Allocate parameters
+  ALLOCATE(NODE_LIST(TOTAL_NUMBER_OF_NODES,3),STAT=ERR)
+  ALLOCATE(SPEED_FUNCTION_TABLE(TOTAL_NUMBER_OF_NODES,3),STAT=ERR)
+  ALLOCATE(CONDUCTIVITY_TENSOR(TOTAL_NUMBER_OF_NODES,9),STAT=ERR)
+  ALLOCATE(CONNECTIVITY_LIST(TOTAL_NUMBER_OF_NODES,50),STAT=ERR)
+  ALLOCATE(ELEMENT_LIST(TOTAL_NUMBER_OF_ELEMENTS,20),STAT=ERR)
+  
+  ALLOCATE(CONNECTIVITY_NUMBER(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(SEED_VALUE(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(SNAPSHOT_SEED_VALUE(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(RELATIVE_ERROR(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(SEED_VALUE_EXACT_SOLUTION(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(STATUS_MASK(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(TRACE_NODE(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(TRACE_NODES(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+
+  ALLOCATE(RAW_INDEX(TOTAL_NUMBER_OF_NODES+1),STAT=ERR)
+  ALLOCATE(COLUMN_INDEX(TOTAL_NUMBER_OF_CONNECTIVITY),STAT=ERR)
+  ALLOCATE(SPEED_FUNCTION_TABLE_ON_CONNECTIVITY(TOTAL_NUMBER_OF_CONNECTIVITY,3),STAT=ERR)
+  ALLOCATE(CONDUCTIVITY_TENSOR_ON_CONNECTIVITY(TOTAL_NUMBER_OF_CONNECTIVITY,9),STAT=ERR)  
+  
+  DO I=1,TOTAL_NUMBER_OF_NODES
+    TRACE_NODES(I)=0.0
+  ENDDO
+
+  MATERIAL_BEHAVIOUR = "ISOTROPIC"
+  INPUT_TYPE_FOR_CONDUCTIVITY = "VECTOR" 
+  INPUT_TYPE_FOR_SEED_VALUE = "LIST"
+
+  SEED_POINT = NODE_2
+  STATUS_MASK(SEED_POINT)="SEED POINT"
+  SEED_VALUE(SEED_POINT)=0.0_CMISSDP
+  TRACE_NODE(SEED_POINT)=0
+
+  INPUT_TYPE_FOR_SPEED_FUNCTION = "FIXED" 
+  SPEED_FUNCTION_ALONG_EIGEN_VECTOR(1) = 1.0_CMISSDP
+
+
+  CALL CMISS_PRE_PROCESS_INFORMATION(MATERIAL_BEHAVIOUR,INPUT_FILE_NAME,INPUT_FILE_FORMAT,TOTAL_NUMBER_OF_NODES,&
+&INPUT_TYPE_FOR_SEED_VALUE,INPUT_TYPE_FOR_SPEED_FUNCTION,SPEED_FUNCTION_ALONG_EIGEN_VECTOR,INPUT_TYPE_FOR_CONDUCTIVITY,&
+&STATUS_MASK,NODE_LIST,CONDUCTIVITY_TENSOR,SPEED_FUNCTION_TABLE,SEED_VALUE,CONNECTIVITY_NUMBER,&
+&SPEED_FUNCTION_TABLE_ON_CONNECTIVITY,CONDUCTIVITY_TENSOR_ON_CONNECTIVITY,RAW_INDEX,COLUMN_INDEX,&
+&TOTAL_NUMBER_OF_CONNECTIVITY,CONNECTIVITY_LIST,ELEMENT_LIST,TOTAL_NUMBER_OF_ELEMENTS,NUMBER_OF_NODES_PER_ELEMENT,Err)
+
+  CALL CMISS_SOLVE_PROBLEM_GEODESIC_CONNECTIVITY(TOTAL_NUMBER_OF_NODES,NODE_LIST,CONDUCTIVITY_TENSOR_ON_CONNECTIVITY,&
+                       &SPEED_FUNCTION_TABLE_ON_CONNECTIVITY,RAW_INDEX,COLUMN_INDEX,TOTAL_NUMBER_OF_CONNECTIVITY,&
+                       &SEED_VALUE,STATUS_MASK,TRACE_NODE)
+
+  OUTPUT_FILE_FORMAT = "VTKTET"
+  OUTPUT_FILE_NAME = "src/Output"
+  OUTPUT_FILE_FIELD_TITLE = "trace"
+      
+  J=NODE_1
+  TRACE_NODES(J)=1.0
+  DO WHILE (J .NE. NODE_2)
+    J=TRACE_NODE(J)
+    TRACE_NODES(J)=1.0
+  ENDDO
+  
+  CALL CMISS_POST_PROCESS_DATA(MATERIAL_BEHAVIOUR,OUTPUT_FILE_NAME,OUTPUT_FILE_FORMAT,TOTAL_NUMBER_OF_NODES,NODE_LIST,&
+&CONDUCTIVITY_TENSOR,SPEED_FUNCTION_TABLE,TRACE_NODES,CONNECTIVITY_NUMBER,OUTPUT_FILE_FIELD_TITLE,&
+&CONNECTIVITY_LIST,ELEMENT_LIST,TOTAL_NUMBER_OF_ELEMENTS,NUMBER_OF_NODES_PER_ELEMENT,Err)
+  
+  STOP
+
+!...................................................................................
+END PROGRAM GeodesicEx1
+!...................................................................................
---
+++ ClassicalField/Hamilton_Jacobi/TwoElmnTet/src/TwoElmnTetExample.f90
@@ -0,0 +1,222 @@
+!> \file
+!> $Id: TwoElmnTetExample.f90 20 2007-05-28 20:22:52Z cpb $
+!> \author Ali PASHAEI
+!> \brief This is an example program to solve a Hamilton-Jacobi (Eikonal form) equation using openCMISS calls.
+!>
+!> \section LICENSE
+!>
+!> Version: 0.1
+!>
+!> The contents of this file are subject to the Mozilla Public License
+!> Version 1.1 (the "License"); you may not use this file except in
+!> compliance with the License. You may obtain a copy of the License at
+!> http://www.mozilla.org/MPL/
+!>
+!> Software distributed under the License is distributed on an "AS IS"
+!> basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+!> License for the specific language governing rights and limitations
+!> under the License.
+!>
+!> The Original Code is openCMISS
+!>
+!> The Initial Developer of the Original Code is  University of Pompeu Fabra,
+!> Barcelona, Spain, University of Auckland, Auckland, New Zealand and 
+!> University of Oxford, Oxford, United Kingdom. Portions created by the 
+!> University of Auckland and University of Oxford are Copyright (C) 2007 
+!> by the University of Pompeu Fabra, the University of Auckland and
+!> the University of Oxford. All Rights Reserved.
+!>
+!> Contributor(s): 
+!>
+!> Alternatively, the contents of this file may be used under the terms of
+!> either the GNU General Public License Version 2 or later (the "GPL"), or
+!> the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+!> in which case the provisions of the GPL or the LGPL are applicable instead
+!> of those above. If you wish to allow use of your version of this file only
+!> under the terms of either the GPL or the LGPL, and not to allow others to
+!> use your version of this file under the terms of the MPL, indicate your
+!> decision by deleting the provisions above and replace them with the notice
+!> and other provisions required by the GPL or the LGPL. If you do not delete
+!> the provisions above, a recipient may use your version of this file under
+!> the terms of any one of the MPL, the GPL or the LGPL.
+!>
+
+!> \example ClassicalField/Hamilton_Jacobi/TwoElmnTet/src/TwoElmnTetExample.f90
+!! Example program to solve a fast electrophysiology equation using openCMISS calls.
+!! \par Latest Builds:
+!! \li <a href='http://autotest.bioeng.auckland.ac.nz/opencmiss-build/logs_x86_64-linux/FiniteElasticity/TwoElementTriLinear/build-intel'>Linux Intel Build</a>
+!! \li <a href='http://autotest.bioeng.auckland.ac.nz/opencmiss-build/logs_x86_64-linux/FiniteElasticity/TwoElementTriLinear/build-gnu'>Linux GNU Build</a>
+!<
+
+!> Main program
+
+!...................................................................................
+PROGRAM TwoElmnTet
+!...................................................................................
+
+! OpenCMISS Modules
+
+  USE OPENCMISS
+
+#ifdef WIN32
+  USE IFQWIN
+#endif
+
+!...................................................................................
+  IMPLICIT NONE
+!...................................................................................
+
+  !Program variables
+
+  INTEGER(CMISSIntg), PARAMETER :: CoordinateSystemUserNumber=1
+  INTEGER(CMISSIntg), PARAMETER :: RegionUserNumber=2
+
+  !local variables
+
+  INTEGER(CMISSIntg) :: I,J,K,ELEMENT_NUMBER,MIN_TRIAL_NODE,TRIAL_STATUS,CHANGED_STATUS,SEED_POINT,TOTAL_NUMBER_OF_CONNECTIVITY
+  INTEGER(CMISSIntg) :: NUMBER_OF_NODES_PER_ELEMENT,TOTAL_NUMBER_OF_NODES,TOTAL_NUMBER_OF_ELEMENTS
+  INTEGER(CMISSIntg) :: NUMBER_OF_SNAPSHOTS
+  REAL(CMISSDP) :: MIN_VALUE,MAX_VALUE,TIME_STEP
+  REAL(CMISSDP) :: SPEED_FUNCTION_ALONG_EIGEN_VECTOR(3)
+  CHARACTER (LEN=100) :: INPUT_FILE_NAME
+  CHARACTER (LEN=100) :: OUTPUT_FILE_NAME
+  CHARACTER (LEN=300) :: OUTPUT_FILE_FIELD_TITLE
+  CHARACTER (LEN=10)  :: INPUT_FILE_FORMAT
+  CHARACTER (LEN=10)  :: OUTPUT_FILE_FORMAT
+  CHARACTER (LEN=12)  :: MATERIAL_BEHAVIOUR
+  CHARACTER (LEN=10)  :: INPUT_TYPE_FOR_SPEED_FUNCTION
+  CHARACTER (LEN=10)  :: INPUT_TYPE_FOR_SEED_VALUE
+  CHARACTER (LEN=10)  :: INPUT_TYPE_FOR_CONDUCTIVITY
+  CHARACTER (LEN=100) :: STRING
+
+  INTEGER(CMISSIntg) :: Err
+
+  TYPE(CMISSCoordinateSystemType) :: CoordinateSystem,WorldCoordinateSystem
+  TYPE(CMISSRegionType) :: WorldRegion
+  
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: NODE_LIST
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: SPEED_FUNCTION_TABLE
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: CONDUCTIVITY_TENSOR
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: SPEED_FUNCTION_TABLE_ON_CONNECTIVITY
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:,:):: CONDUCTIVITY_TENSOR_ON_CONNECTIVITY
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: SEED_VALUE
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: SNAPSHOT_SEED_VALUE
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: RELATIVE_ERROR
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: SEED_VALUE_EXACT_SOLUTION
+  REAL(CMISSDP), ALLOCATABLE, DIMENSION(:)  :: TRACE_NODES
+  
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:,:):: CONNECTIVITY_LIST
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:,:):: ELEMENT_LIST
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: CONNECTIVITY_NUMBER
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: TRACE_NODE
+
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: COLUMN_INDEX
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: TEMP_ARRAY
+  INTEGER(CMISSIntg), ALLOCATABLE, DIMENSION(:)  :: RAW_INDEX
+
+  CHARACTER (LEN=10), ALLOCATABLE, DIMENSION(:)  :: STATUS_MASK
+
+#ifdef WIN32
+  !Initialise QuickWin
+  QUICKWIN_WINDOW_CONFIG%TITLE="General Output" !Window title
+  QUICKWIN_WINDOW_CONFIG%NUMTEXTROWS=-1 !Max possible number of rows
+  QUICKWIN_WINDOW_CONFIG%MODE=QWIN$SCROLLDOWN
+  QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
+  IF(.NOT.QUICKWIN_STATUS) QUICKWIN_STATUS=SETWINDOWCONFIG(QUICKWIN_WINDOW_CONFIG)
+#endif
+
+  !Intialise OpenCMISS
+  CALL CMISSInitialise(WorldCoordinateSystem,WorldRegion,Err)
+  CALL CMISSCoordinateSystemTypeInitialise(CoordinateSystem,Err)
+  CALL CMISSCoordinateSystemCreateStart(CoordinateSystemUserNumber,CoordinateSystem,Err)
+  CALL CMISSCoordinateSystemDimensionSet(CoordinateSystem,3,Err)
+  CALL CMISSCoordinateSystemCreateFinish(CoordinateSystem,Err)
+
+! INPUT_FILE_FORMAT :   "TABC"   -> tabc format
+!			"TETGEN" -> tetgen format
+  INPUT_FILE_FORMAT  = "TETGEN"
+
+! INPUT_FILE_NAME :   in order to calculate TOTAL_NUMBER_OF_NODES
+  INPUT_FILE_NAME = "src/TwoElmnTet"
+
+! TOTAL_NUMBER_OF_NODES : to set input parameters
+  CALL CMISS_NUMBER_OF_INPUT_NODES(INPUT_FILE_NAME,INPUT_FILE_FORMAT,TOTAL_NUMBER_OF_NODES,TOTAL_NUMBER_OF_ELEMENTS,&
+  & TOTAL_NUMBER_OF_CONNECTIVITY,Err)
+!  Allocate parameters
+  ALLOCATE(NODE_LIST(TOTAL_NUMBER_OF_NODES,3),STAT=ERR)
+  ALLOCATE(SPEED_FUNCTION_TABLE(TOTAL_NUMBER_OF_NODES,3),STAT=ERR)
+  ALLOCATE(CONDUCTIVITY_TENSOR(TOTAL_NUMBER_OF_NODES,9),STAT=ERR)
+  ALLOCATE(CONNECTIVITY_LIST(TOTAL_NUMBER_OF_NODES,50),STAT=ERR)
+  ALLOCATE(ELEMENT_LIST(TOTAL_NUMBER_OF_ELEMENTS,20),STAT=ERR)
+  
+  ALLOCATE(CONNECTIVITY_NUMBER(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(SEED_VALUE(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(SNAPSHOT_SEED_VALUE(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(RELATIVE_ERROR(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(SEED_VALUE_EXACT_SOLUTION(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(STATUS_MASK(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(TRACE_NODE(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+  ALLOCATE(TRACE_NODES(TOTAL_NUMBER_OF_NODES),STAT=ERR)
+
+  ALLOCATE(RAW_INDEX(TOTAL_NUMBER_OF_NODES+1),STAT=ERR)
+  ALLOCATE(COLUMN_INDEX(TOTAL_NUMBER_OF_CONNECTIVITY),STAT=ERR)
+  ALLOCATE(SPEED_FUNCTION_TABLE_ON_CONNECTIVITY(TOTAL_NUMBER_OF_CONNECTIVITY,3),STAT=ERR)
+  ALLOCATE(CONDUCTIVITY_TENSOR_ON_CONNECTIVITY(TOTAL_NUMBER_OF_CONNECTIVITY,9),STAT=ERR)  
+  
+! MATERIAL_BEHAVIOUR : "ISOTROPIC"   -> isotropic material properties
+!		               "ANISOTROPIC" -> anisotropic material properties
+  MATERIAL_BEHAVIOUR = "ANISOTROPIC"
+
+! if material is ANISOTROPIC
+! INPUT_TYPE_FOR_CONDUCTIVITY : "TENSOR" -> three EigenVectors
+!		                        "VECTOR" -> first EigenVector
+  INPUT_TYPE_FOR_CONDUCTIVITY = "VECTOR" 
+
+! INPUT_TYPE_FOR_SEED_VALUE : "FILE"  -> refers to file extension ".ESTM" with the same "INPUT_FILE_NAME" name
+!		                      "LIST"  -> refers to NODAL LIST below
+  INPUT_TYPE_FOR_SEED_VALUE = "LIST"
+
+    ! SEED POINT 2
+    SEED_POINT = 2
+    STATUS_MASK(SEED_POINT)="SEED POINT"
+    SEED_VALUE(SEED_POINT)=0.0_CMISSDP
+
+! INPUT_TYPE_FOR_SPEED_FUNCTION : "FILE"   -> refers to file extension ".COND" with the same "INPUT_FILE_NAME" name
+!		                          "FIXED"  -> refers to constant values below
+  INPUT_TYPE_FOR_SPEED_FUNCTION = "FIXED" 
+
+    ! enter the SPEED FUNCTIONS along EIGEN-VECTORS
+    ! EIGEN-VECTOR 1
+    SPEED_FUNCTION_ALONG_EIGEN_VECTOR(1) = 10.0_CMISSDP
+
+    ! EIGEN-VECTOR 2
+    SPEED_FUNCTION_ALONG_EIGEN_VECTOR(2) = 1.0_CMISSDP
+
+    ! EIGEN-VECTOR 3
+    SPEED_FUNCTION_ALONG_EIGEN_VECTOR(3) = 1.0_CMISSDP
+
+  CALL CMISS_PRE_PROCESS_INFORMATION(MATERIAL_BEHAVIOUR,INPUT_FILE_NAME,INPUT_FILE_FORMAT,TOTAL_NUMBER_OF_NODES,&
+&INPUT_TYPE_FOR_SEED_VALUE,INPUT_TYPE_FOR_SPEED_FUNCTION,SPEED_FUNCTION_ALONG_EIGEN_VECTOR,INPUT_TYPE_FOR_CONDUCTIVITY,&
+&STATUS_MASK,NODE_LIST,CONDUCTIVITY_TENSOR,SPEED_FUNCTION_TABLE,SEED_VALUE,CONNECTIVITY_NUMBER,&
+&SPEED_FUNCTION_TABLE_ON_CONNECTIVITY,CONDUCTIVITY_TENSOR_ON_CONNECTIVITY,RAW_INDEX,COLUMN_INDEX,TOTAL_NUMBER_OF_CONNECTIVITY,&
+&CONNECTIVITY_LIST,ELEMENT_LIST,TOTAL_NUMBER_OF_ELEMENTS,NUMBER_OF_NODES_PER_ELEMENT,Err)
+
+  CALL CMISS_SOLVE_PROBLEM_FMM_CONNECTIVITY(TOTAL_NUMBER_OF_NODES,NODE_LIST,CONDUCTIVITY_TENSOR_ON_CONNECTIVITY,&
+                       &SPEED_FUNCTION_TABLE_ON_CONNECTIVITY,RAW_INDEX,COLUMN_INDEX,TOTAL_NUMBER_OF_CONNECTIVITY,&
+                       &SEED_VALUE,STATUS_MASK)
+
+! OUTPUT_FILE_FORMAT :  "TABC" -> tabc format
+!			"VTKTET"  -> VTK tetrahedral format
+  OUTPUT_FILE_FORMAT = "VTKTET"
+  OUTPUT_FILE_NAME = "src/Output"
+  OUTPUT_FILE_FIELD_TITLE = "depolarizatin_time_comp"
+
+  CALL CMISS_POST_PROCESS_DATA(MATERIAL_BEHAVIOUR,OUTPUT_FILE_NAME,OUTPUT_FILE_FORMAT,TOTAL_NUMBER_OF_NODES,NODE_LIST,&
+&CONDUCTIVITY_TENSOR,SPEED_FUNCTION_TABLE,SEED_VALUE,CONNECTIVITY_NUMBER,OUTPUT_FILE_FIELD_TITLE,&
+&CONNECTIVITY_LIST,ELEMENT_LIST,TOTAL_NUMBER_OF_ELEMENTS,NUMBER_OF_NODES_PER_ELEMENT,Err)
+  
+  STOP
+
+!...................................................................................
+END PROGRAM TwoElmnTet
+!...................................................................................
