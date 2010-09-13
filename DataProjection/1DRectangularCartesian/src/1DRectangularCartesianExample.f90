471dc0866ae2beaca3a0adaa58d8c37583529acf
Tim Wu <twu051@aucklanduni.ac.nz>
2010-09-13 05:44:51 +0000

    fixing small bugs relating to set projection tolerance, and maximum iterations number, and add these routine calls into the 1D example
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1477 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- DataProjection/1DRectangularCartesian/src/1DRectangularCartesianExample.f90
+++ DataProjection/1DRectangularCartesian/src/1DRectangularCartesianExample.f90
@@ -1,3 +1,44 @@
+!> \file
+!> $Id: 1DRectangularCartesianExample.f90 $
+!> \author Tim Wu
+!> \brief This is an example program to solve 3D data points projecting onto 1D cartesian elements using openCMISS calls.
+!>
+!> \section LICENSE
+!>
+!> Version: MPL 1.1/GPL 2.0/LGPL 2.1
+!>
+!> The contents of this file are subject to the Mozilla Public License
+!> Version 1.1 (the "License"); you may not use this file except in
+!> compliance with the License. You may obtain a copy of the License at
+!> http://www.mozilla.org/MPL/
+!>
+!> Software distributed under the License is distributed on an "AS IS"
+!> basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+!> License for the specific language governing rights and limitations
+!> under the License.
+!>
+!> The Original Code is openCMISS
+!>
+!> The Initial Developer of the Original Code is University of Auckland,
+!> Auckland, New Zealand and University of Oxford, Oxford, United
+!> Kingdom. Portions created by the University of Auckland and University
+!> of Oxford are Copyright (C) 2007 by the University of Auckland and
+!> the University of Oxford. All Rights Reserved.
+!>
+!> Contributor(s): Code based on the examples by Kumar Mithraratne and Prasad Babarenda Gamage
+!>
+!> Alternatively, the contents of this file may be used under the terms of
+!> either the GNU General Public License Version 2 or later (the "GPL"), or
+!> the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+!> in which case the provisions of the GPL or the LGPL are applicable instead
+!> of those above. If you wish to allow use of your version of this file only
+!> under the terms of either the GPL or the LGPL, and not to allow others to
+!> use your version of this file under the terms of the MPL, indicate your
+!> decision by deleting the provisions above and replace them with the notice
+!> and other provisions required by the GPL or the LGPL. If you do not delete
+!> the provisions above, a recipient may use your version of this file under
+!> the terms of any one of the MPL, the GPL or the LGPL.
+
 !> Main program
 PROGRAM DataProjection1DRectangularCartesian
 
@@ -35,7 +76,7 @@ PROGRAM DataProjection1DRectangularCartesian
   INTEGER(CMISSIntg) :: WorldRegionUserNumber
   
   INTEGER(CMISSIntg) :: FieldNumberOfVariables=1
-  INTEGER(CMISSIntg) :: FieldNumberOfComponents=3 
+  INTEGER(CMISSIntg) :: FieldNumberOfComponents=3
   
 
   INTEGER(CMISSIntg) :: np,el,xi,der_idx,node_idx,comp_idx
@@ -43,7 +84,22 @@ PROGRAM DataProjection1DRectangularCartesian
   REAL(CMISSDP), DIMENSION(5,3) :: DataPointValues !(number_of_data_points,dimension)
   INTEGER(CMISSIntg), DIMENSION(5,2) :: ElementUserNodes  
   REAL(CMISSDP), DIMENSION(2,6,3) :: FieldValues
-        
+  
+  !Test variables
+  REAL(CMISSDP) :: AbsoluteToleranceSet=1.0E-10_CMISSDP !default is 1.0E-8
+  REAL(CMISSDP) :: RelativeToleranceSet=1.0E-6_CMISSDP !default is 1.0E-8
+  INTEGER(CMISSIntg) :: MaximumNumberOfIterationsSet=30 !default is 25
+  REAL(CMISSDP) :: MaximumIterationUpdateSet=0.4_CMISSDP !default is 0.5
+  INTEGER(CMISSIntg) :: NumberOfClosestElementsSet=3 !default is 2/4/8 for 1/2/3 dimensional projection 
+  INTEGER(CMISSIntg) :: ProjectionTypeSet=CMISSDataProjectionAllElementsProjectionType !same as default
+  REAL(CMISSDP) :: StartingXiSet(1)=(/0.4_CMISSDP/) !default is 0.5
+  REAL(CMISSDP) :: AbsoluteToleranceGet
+  REAL(CMISSDP) :: RelativeToleranceGet
+  INTEGER(CMISSIntg) :: MaximumNumberOfIterationsGet
+  REAL(CMISSDP) :: MaximumIterationUpdateGet
+  INTEGER(CMISSIntg) :: NumberOfClosestElementsGet
+  INTEGER(CMISSIntg) :: ProjectionTypeGet
+  REAL(CMISSDP), ALLOCATABLE :: StartingXiGet(:)
 #ifdef WIN32
   !Quickwin type
   LOGICAL :: QUICKWIN_STATUS=.FALSE.
@@ -183,9 +239,28 @@ PROGRAM DataProjection1DRectangularCartesian
   
   !=========================================================================================================================
   !Create a data projection
-  
   CALL CMISSDataProjectionCreateStart(RegionUserNumber,FieldUserNumber,RegionUserNumber,Err)
+  !=========================================================================================================================
+  !Test parameter set functions
+  CALL CMISSDataProjectionAbsoluteToleranceSet(RegionUserNumber,AbsoluteToleranceSet,Err) !test
+  CALL CMISSDataProjectionMaximumIterationUpdateSet(RegionUserNumber,MaximumIterationUpdateSet,Err) !test
+  CALL CMISSDataProjectionMaximumNumberOfIterationsSet(RegionUserNumber,MaximumNumberOfIterationsSet,Err) !test
+  CALL CMISSDataProjectionNumberOfClosestElementsSet(RegionUserNumber,NumberOfClosestElementsSet,Err) !test
+  CALL CMISSDataProjectionProjectionTypeSet(RegionUserNumber,ProjectionTypeSet,Err)
+  CALL CMISSDataProjectionRelativeToleranceSet(RegionUserNumber,RelativeToleranceSet,Err) !test
+  CALL CMISSDataProjectionStartingXiSet(RegionUserNumber,StartingXiSet,Err) !test
+  !=========================================================================================================================
+  !Finish data projection  
   CALL CMISSDataProjectionCreateFinish(RegionUserNumber,Err)
+  !=========================================================================================================================
+  !Test parameter get functions
+  CALL CMISSDataProjectionAbsoluteToleranceGet(RegionUserNumber,AbsoluteToleranceGet,Err) !test
+  CALL CMISSDataProjectionMaximumIterationUpdateGet(RegionUserNumber,MaximumIterationUpdateGet,Err) !test
+  CALL CMISSDataProjectionMaximumNumberOfIterationsGet(RegionUserNumber,MaximumNumberOfIterationsGet,Err) !test
+  CALL CMISSDataProjectionNumberOfClosestElementsGet(RegionUserNumber,NumberOfClosestElementsGet,Err) !test
+  CALL CMISSDataProjectionProjectionTypeGet(RegionUserNumber,ProjectionTypeGet,Err) !test
+  CALL CMISSDataProjectionRelativeToleranceGet(RegionUserNumber,RelativeToleranceGet,Err) !test
+  CALL CMISSDataProjectionStartingXiGet(RegionUserNumber,StartingXiGet,Err) !test  
   
   !=========================================================================================================================
   !Start data projection
