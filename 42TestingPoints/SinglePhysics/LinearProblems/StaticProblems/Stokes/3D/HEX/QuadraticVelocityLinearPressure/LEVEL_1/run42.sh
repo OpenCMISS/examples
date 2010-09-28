7f259ded0e752e28b4887df16681157863da965c
Sebastian Krittian <sebastian.krittian@comlab.ox.ac.uk>
2010-09-28 15:24:54 +0000

    Added $OPENCMISS_ROOT to shell script (developers can point to their examples).
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1572 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- 42TestingPoints/SinglePhysics/LinearProblems/StaticProblems/Stokes/3D/HEX/QuadraticVelocityLinearPressure/LEVEL_1/run42.sh
+++ 42TestingPoints/SinglePhysics/LinearProblems/StaticProblems/Stokes/3D/HEX/QuadraticVelocityLinearPressure/LEVEL_1/run42.sh
@@ -1,3 +1,2 @@
 #! /bin/bash
-./bin/x86_64-linux/mpich2/gnu/LEVEL_1Example-debug
-
+$OPENCMISS_ROOT/cm/examples/42TestingPoints/SinglePhysics/LinearProblems/StaticProblems/Stokes/3D/HEX/QuadraticVelocityLinearPressure/LEVEL_1/bin/x86_64-linux/mpich2/gnu/LEVEL_1Example
