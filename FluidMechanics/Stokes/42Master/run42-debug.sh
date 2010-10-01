8e0fd43021e9811f966014f7d11ad2af377e699f
Sebastian Krittian <sebastian.krittian@comlab.ox.ac.uk>
2010-10-01 09:41:19 +0000

    Fluid mechanics examples need to be executed in local directories to store output correctly.
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1591 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- FluidMechanics/Stokes/42Master/run42-debug.sh
+++ FluidMechanics/Stokes/42Master/run42-debug.sh
@@ -1,1 +1,2 @@
-${OPENCMISS_ROOT}/cm/examples/42TestingPoints/SinglePhysics/LinearProblems/StaticProblems/Stokes/2D/QUAD/CubicVelocityLinearPressure/LEVEL_1/run42-debug.sh
+cd ${OPENCMISS_ROOT}/cm/examples/42TestingPoints/SinglePhysics/LinearProblems/StaticProblems/Stokes/2D/QUAD/CubicVelocityLinearPressure/LEVEL_1/
+./run42-debug.sh
