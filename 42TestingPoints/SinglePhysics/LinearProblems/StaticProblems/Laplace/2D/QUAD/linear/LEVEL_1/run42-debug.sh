e94f94c91c44e256117a3393b826202cddc21a45
Adam Reeve <aree035@aucklanduni.ac.nz>
2010-10-14 23:11:02 +0000

    Removing the command to move the diagnostics output from the run42 scripts. I turned the diagnostics off as for the large examples they generated huge files that weren't being used.
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1642 61e36070-c886-4827-b7c0-ed9e4dc59cc8

--- 42TestingPoints/SinglePhysics/LinearProblems/StaticProblems/Laplace/2D/QUAD/linear/LEVEL_1/run42-debug.sh
+++ 42TestingPoints/SinglePhysics/LinearProblems/StaticProblems/Laplace/2D/QUAD/linear/LEVEL_1/run42-debug.sh
@@ -1,4 +1,3 @@
 #! /bin/bash
 $OPENCMISS_ROOT/cm/examples/ClassicalField/Laplace/42Master/bin/x86_64-linux/mpich2/gnu/42MasterExample-debug -2D -quad -linearbasis -nx 10 -ny 10
 mv Laplace* output/
-mv Diagnostics.diag output/
