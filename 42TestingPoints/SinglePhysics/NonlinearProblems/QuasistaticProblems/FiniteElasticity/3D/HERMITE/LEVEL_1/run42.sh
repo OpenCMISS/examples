97a452b26805c631be17601f3d5340d2e3d022b9
Jack Lee <jack.lee@kcl.ac.uk>
2010-11-19 21:07:58 +0000

    T1227,1211 Finite Elasticity testing points expected results added
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1758 61e36070-c886-4827-b7c0-ed9e4dc59cc8

---
+++ 42TestingPoints/SinglePhysics/NonlinearProblems/QuasistaticProblems/FiniteElasticity/3D/HERMITE/LEVEL_1/run42.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+$OPENCMISS_ROOT/cm/examples/FiniteElasticity/testingPoints/bin/x86_64-linux/mpich2/gnu/testingPointsExample  -DIM=3D -ELEM=HERMITE -BASIS=hermite -LEVEL=1
+mv *.exnode *.exelem output/
