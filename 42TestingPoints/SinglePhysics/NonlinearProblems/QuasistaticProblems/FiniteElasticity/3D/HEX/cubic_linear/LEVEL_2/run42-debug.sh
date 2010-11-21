259f692904e9834d9e0ebfc6c7195ab07f564dc9
Jack Lee <jack.lee@kcl.ac.uk>
2010-11-21 14:26:22 +0000

    t1227,1211 adding debug version testing shell scripts
    git-svn-id: https://opencmiss.svn.sourceforge.net/svnroot/opencmiss/cm/trunk@1763 61e36070-c886-4827-b7c0-ed9e4dc59cc8

---
+++ 42TestingPoints/SinglePhysics/NonlinearProblems/QuasistaticProblems/FiniteElasticity/3D/HEX/cubic_linear/LEVEL_2/run42-debug.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+$OPENCMISS_ROOT/cm/examples/FiniteElasticity/testingPoints/bin/x86_64-linux/mpich2/gnu/testingPointsExample-debug  -DIM=3D -ELEM=HEX -BASIS_1=cubic -BASIS_2=linear -LEVEL=2
+mv *.exnode *.exelem output/
