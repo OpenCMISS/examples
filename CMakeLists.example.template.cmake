326d9125e5b92ea8992d075f38f922f73ea59959
Daniel Wirtz <daniel.wirtz@simtech.uni-stuttgart.de>
2015-04-10 14:24:34 +0200

    some examples are now building!

--- CMakeLists.example.template.cmake
+++ CMakeLists.example.template.cmake
@@ -1,8 +1,9 @@
 cmake_minimum_required(VERSION 3.0)
 # Get lowercase name
-string(TOLOWER "@EXAMPLE_NAME@" EXAMPLE_NAME)
+#string(TOLOWER "@EXAMPLE_NAME@" EXAMPLE_NAME)
+set(EXAMPLE_TARGET run)
 
-project(OpenCMISSExample-${EXAMPLE_NAME} LANGUAGES C CXX Fortran VERSION 1.0)
+project(OpenCMISSExample LANGUAGES C CXX Fortran VERSION 1.0)
 SET(CMAKE_NO_SYSTEM_FROM_IMPORTED YES)
 
 # Have CMake find the package components
@@ -27,7 +28,9 @@ find_package(SUPERLU_DIST REQUIRED)
 # Get sources in /src
 file(GLOB SRC src/*.f90 src/*.c ../input/*.f90)
 # Add example executable
-add_executable(${EXAMPLE_NAME} ${SRC})
+add_executable(${EXAMPLE_TARGET} ${SRC})
 # Link to iron - contains forward refs to all other necessary libs
-target_link_libraries(${EXAMPLE_NAME} PUBLIC iron)
+target_link_libraries(${EXAMPLE_TARGET} PUBLIC iron)
 set(CMAKE_Fortran_FLAGS "-cpp")
+
+install(TARGETS ${EXAMPLE_TARGET} DESTINATION .)
