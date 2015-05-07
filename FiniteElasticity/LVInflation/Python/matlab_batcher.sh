416f5522a0928814170abee273dff820b54e3fb4
jennyhelyanwe <jenny.helyanw@gmail.com>
2015-05-07 13:59:05 +1200

    Remove matlab related files

--- FiniteElasticity/LVInflation/Python/matlab_batcher.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-matlab_exec=matlab
-X="${1}(${2})"
-echo ${X} > matlab_command_${2}.m
-cat matlab_command_${2}.m
-${matlab_exec} -nojvm -nodisplay -nosplash < matlab_command_${2}.m
-rm matlab_command_${2}.m
-
