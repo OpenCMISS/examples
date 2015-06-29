384aabb74fa02c5f239e40db670d847a9a59b0cf
David Nickerson <nickerso@users.sourceforge.net>
2015-06-29 17:17:28 +1200

    adding another example using my simple sine test model which does integrate successfully using Iron with Euler; changing integration time step size has expected effect on integration results

---
+++ cellml/model-integration/Python/sine-integration.py
@@ -0,0 +1,359 @@
+#!/usr/bin/env python
+
+#DOC-START imports
+import sys, os, math
+# Make sure $OPENCMISS_ROOT/cm/bindings/python is first in our PYTHONPATH.
+sys.path.insert(1, os.path.join((os.environ['OPENCMISS_ROOT'],'cm','bindings','python')))
+
+# Intialise OpenCMISS
+from opencmiss import CMISS
+#DOC-END imports
+
+# Set problem parameters
+#DOC-START parameters
+
+# get the model file
+if len(sys.argv) > 1:
+    modelFile = sys.argv[1]
+else:
+    modelFile = "https://models.physiomeproject.org/w/andre/sine/rawfile/654722543efff0fb045839758f33bcf438d26979/sin_approximations.xml"
+
+# 1D domain size
+width = 1.0
+numberOfXElements = 2
+
+# Materials parameters
+Am = 193.6
+Cm = 0.014651
+conductivity = 0.1
+
+# Simulation parameters
+stimValue = 0.0
+stimStop = 0.0
+timeStop = 7.0 # [s]
+# this is used in the integration of the CellML model
+odeTimeStep = 0.001 # [s]
+# this is used in the dummy monodomain problem/solver
+pdeTimeStep = 10.0 # [s]
+# this is the step at which we grab output from the solver
+outputTimeStep = 1.0
+# set this to 1 to get exfiles written out during solve
+outputFrequency = 0
+#DOC-END parameters
+
+#Setup field number handles
+coordinateSystemUserNumber = 1
+regionUserNumber = 1
+basisUserNumber = 1
+pressureBasisUserNumber = 2
+generatedMeshUserNumber = 1
+meshUserNumber = 1
+cellMLUserNumber = 1
+decompositionUserNumber = 1
+equationsSetUserNumber = 1
+problemUserNumber = 1
+#Mesh component numbers
+linearMeshComponentNumber = 1
+#Fields
+geometricFieldUserNumber = 1
+fibreFieldUserNumber = 2
+dependentFieldUserNumber = 3
+materialsFieldUserNumber = 4
+equationsSetFieldUserNumber = 5
+cellMLModelsFieldUserNumber = 6
+cellMLStateFieldUserNumber = 7
+cellMLParametersFieldUserNumber = 8
+cellMLIntermediateFieldUserNumber = 9
+
+#DOC-START parallel information
+# Get the number of computational nodes and this computational node number
+numberOfComputationalNodes = CMISS.ComputationalNumberOfNodesGet()
+computationalNodeNumber = CMISS.ComputationalNodeNumberGet()
+#DOC-END parallel information
+
+#DOC-START initialisation
+# Create a 2D rectangular cartesian coordinate system
+coordinateSystem = CMISS.CoordinateSystem()
+coordinateSystem.CreateStart(coordinateSystemUserNumber)
+coordinateSystem.DimensionSet(1)
+coordinateSystem.CreateFinish()
+
+# Create a region and assign the coordinate system to the region
+region = CMISS.Region()
+region.CreateStart(regionUserNumber,CMISS.WorldRegion)
+region.LabelSet("Region")
+region.coordinateSystem = coordinateSystem
+region.CreateFinish()
+#DOC-END initialisation
+
+#DOC-START basis
+# Define a bilinear Lagrange basis
+basis = CMISS.Basis()
+basis.CreateStart(basisUserNumber)
+basis.type = CMISS.BasisTypes.LAGRANGE_HERMITE_TP
+basis.numberOfXi = 1
+basis.interpolationXi = [CMISS.BasisInterpolationSpecifications.LINEAR_LAGRANGE]
+basis.CreateFinish()
+#DOC-END basis
+
+#DOC-START generated mesh
+# Create a generated mesh
+generatedMesh = CMISS.GeneratedMesh()
+generatedMesh.CreateStart(generatedMeshUserNumber,region)
+generatedMesh.type = CMISS.GeneratedMeshTypes.REGULAR
+generatedMesh.basis = [basis]
+generatedMesh.extent = [width]
+generatedMesh.numberOfElements = [numberOfXElements]
+
+mesh = CMISS.Mesh()
+generatedMesh.CreateFinish(meshUserNumber,mesh)
+#DOC-END generated mesh
+
+#DOC-START decomposition
+# Create a decomposition for the mesh
+decomposition = CMISS.Decomposition()
+decomposition.CreateStart(decompositionUserNumber,mesh)
+decomposition.type = CMISS.DecompositionTypes.CALCULATED
+decomposition.numberOfDomains = numberOfComputationalNodes
+decomposition.CreateFinish()
+#DOC-END decomposition
+
+#DOC-START geometry
+# Create a field for the geometry
+geometricField = CMISS.Field()
+geometricField.CreateStart(geometricFieldUserNumber, region)
+geometricField.meshDecomposition = decomposition
+geometricField.TypeSet(CMISS.FieldTypes.GEOMETRIC)
+geometricField.VariableLabelSet(CMISS.FieldVariableTypes.U, "coordinates")
+geometricField.ComponentMeshComponentSet(CMISS.FieldVariableTypes.U, 1, linearMeshComponentNumber)
+geometricField.CreateFinish()
+
+# Set geometry from the generated mesh
+generatedMesh.GeometricParametersCalculate(geometricField)
+#DOC-END geometry
+
+#DOC-START equations set
+# Create the equations_set
+equationsSetField = CMISS.Field()
+equationsSet = CMISS.EquationsSet()
+equationsSet.CreateStart(equationsSetUserNumber, region, geometricField,
+        CMISS.EquationsSetClasses.BIOELECTRICS,
+        CMISS.EquationsSetTypes.MONODOMAIN_EQUATION,
+        CMISS.EquationsSetSubtypes.NONE,
+        equationsSetFieldUserNumber, equationsSetField)
+equationsSet.CreateFinish()
+#DOC-END equations set
+
+#DOC-START equations set fields
+# Create the dependent Field
+dependentField = CMISS.Field()
+equationsSet.DependentCreateStart(dependentFieldUserNumber, dependentField)
+equationsSet.DependentCreateFinish()
+
+# Create the materials Field
+materialsField = CMISS.Field()
+equationsSet.MaterialsCreateStart(materialsFieldUserNumber, materialsField)
+equationsSet.MaterialsCreateFinish()
+
+# Set the materials values
+# Set Am
+materialsField.ComponentValuesInitialise(CMISS.FieldVariableTypes.U,CMISS.FieldParameterSetTypes.VALUES,1,Am)
+# Set Cm
+materialsField.ComponentValuesInitialise(CMISS.FieldVariableTypes.U,CMISS.FieldParameterSetTypes.VALUES,2,Cm)
+# Set conductivity
+materialsField.ComponentValuesInitialise(CMISS.FieldVariableTypes.U,CMISS.FieldParameterSetTypes.VALUES,3,conductivity)
+#DOC-END equations set fields
+
+#DOC-START create cellml environment
+# Create the CellML environment
+cellML = CMISS.CellML()
+cellML.CreateStart(cellMLUserNumber, region)
+# Import a Nobel 98 cell model from a file
+noble98Model = cellML.ModelImport(modelFile)
+#DOC-END create cellml environment
+
+#DOC-START flag variables
+# Now we have imported the model we are able to specify which variables from the model we want to set from openCMISS
+#cellML.VariableSetAsKnown(noble98Model, "fast_sodium_current/g_Na")
+cellML.VariableSetAsKnown(noble98Model, "main/deriv_approx_initial_value")
+# and variables to get from the CellML 
+cellML.VariableSetAsWanted(noble98Model, "main/sin1")
+cellML.VariableSetAsWanted(noble98Model, "main/sin3")
+#DOC-END flag variables
+
+#DOC-START create cellml finish
+cellML.CreateFinish()
+#DOC-END create cellml finish
+
+#DOC-START map Vm components
+# Start the creation of CellML <--> OpenCMISS field maps
+cellML.FieldMapsCreateStart()
+#Now we can set up the field variable component <--> CellML model variable mappings.
+#Map Vm
+cellML.CreateFieldToCellMLMap(dependentField,CMISS.FieldVariableTypes.U,1, CMISS.FieldParameterSetTypes.VALUES,noble98Model,"deriv_approx_sin/sin", CMISS.FieldParameterSetTypes.VALUES)
+cellML.CreateCellMLToFieldMap(noble98Model,"deriv_approx_sin/sin", CMISS.FieldParameterSetTypes.VALUES,dependentField,CMISS.FieldVariableTypes.U,1,CMISS.FieldParameterSetTypes.VALUES)
+
+#Finish the creation of CellML <--> OpenCMISS field maps
+cellML.FieldMapsCreateFinish()
+
+# Set the initial Vm values
+dependentField.ComponentValuesInitialise(CMISS.FieldVariableTypes.U, CMISS.FieldParameterSetTypes.VALUES, 1, 0.0)
+#DOC-END map Vm components
+
+#DOC-START define CellML models field
+#Create the CellML models field
+cellMLModelsField = CMISS.Field()
+cellML.ModelsFieldCreateStart(cellMLModelsFieldUserNumber, cellMLModelsField)
+cellML.ModelsFieldCreateFinish()
+#DOC-END define CellML models field
+
+#DOC-START define CellML state field
+#Create the CellML state field 
+cellMLStateField = CMISS.Field()
+cellML.StateFieldCreateStart(cellMLStateFieldUserNumber, cellMLStateField)
+cellML.StateFieldCreateFinish()
+#DOC-END define CellML state field
+
+#DOC-START define CellML parameters and intermediate fields
+#Create the CellML parameters field 
+cellMLParametersField = CMISS.Field()
+cellML.ParametersFieldCreateStart(cellMLParametersFieldUserNumber, cellMLParametersField)
+cellML.ParametersFieldCreateFinish()
+
+#  Create the CellML intermediate field 
+cellMLIntermediateField = CMISS.Field()
+cellML.IntermediateFieldCreateStart(cellMLIntermediateFieldUserNumber, cellMLIntermediateField)
+cellML.IntermediateFieldCreateFinish()
+#DOC-END define CellML parameters and intermediate fields
+
+# Create equations
+equations = CMISS.Equations()
+equationsSet.EquationsCreateStart(equations)
+equations.sparsityType = CMISS.EquationsSparsityTypes.SPARSE
+equations.outputType = CMISS.EquationsOutputTypes.NONE
+equationsSet.EquationsCreateFinish()
+
+# Find the domains of the first and last nodes
+firstNodeNumber = 1
+lastNodeNumber = (numberOfXElements+1)
+firstNodeDomain = decomposition.NodeDomainGet(firstNodeNumber, 1)
+lastNodeDomain = decomposition.NodeDomainGet(lastNodeNumber, 1)
+
+# set up the indices of the fields we want to grab
+sin1_Component = cellML.FieldComponentGet(noble98Model, CMISS.CellMLFieldTypes.INTERMEDIATE, "main/sin1")
+sin2_Component = cellML.FieldComponentGet(noble98Model, CMISS.CellMLFieldTypes.STATE, "main/sin2")
+sin3_Component = cellML.FieldComponentGet(noble98Model, CMISS.CellMLFieldTypes.INTERMEDIATE, "main/sin3")
+# and the arrays to store them in
+sin1 = []
+sin2 = []
+sin3 = []
+
+# We are using node 1 as the point in our dummy monodomain problem to integrate the CellML model
+cellmlNode = 2
+cellmlNodeDomain = decomposition.NodeDomainGet(cellmlNode, 1)
+cellmlNodeThisComputationalNode = False
+if cellmlNodeDomain == computationalNodeNumber:
+    cellmlNodeThisComputationalNode = True
+
+#DOC-START define monodomain problem
+#Define the problem
+problem = CMISS.Problem()
+problem.CreateStart(problemUserNumber)
+problem.SpecificationSet(CMISS.ProblemClasses.BIOELECTRICS,
+    CMISS.ProblemTypes.MONODOMAIN_EQUATION,
+    CMISS.ProblemSubTypes.MONODOMAIN_GUDUNOV_SPLIT)
+problem.CreateFinish()
+#DOC-END define monodomain problem
+
+#Create the problem control loop
+problem.ControlLoopCreateStart()
+controlLoop = CMISS.ControlLoop()
+problem.ControlLoopGet([CMISS.ControlLoopIdentifiers.NODE],controlLoop)
+controlLoop.TimesSet(0.0,timeStop,pdeTimeStep)
+
+# controlLoop.OutputTypeSet(CMISS.ControlLoopOutputTypes.TIMING)
+controlLoop.OutputTypeSet(CMISS.ControlLoopOutputTypes.NONE)
+
+controlLoop.TimeOutputSet(outputFrequency)
+problem.ControlLoopCreateFinish()
+
+#Create the problem solvers
+daeSolver = CMISS.Solver()
+dynamicSolver = CMISS.Solver()
+problem.SolversCreateStart()
+# Get the first DAE solver
+problem.SolverGet([CMISS.ControlLoopIdentifiers.NODE],1,daeSolver)
+daeSolver.DAETimeStepSet(odeTimeStep)
+daeSolver.OutputTypeSet(CMISS.SolverOutputTypes.NONE)
+# Get the second dynamic solver for the parabolic problem
+problem.SolverGet([CMISS.ControlLoopIdentifiers.NODE],2,dynamicSolver)
+dynamicSolver.OutputTypeSet(CMISS.SolverOutputTypes.NONE)
+problem.SolversCreateFinish()
+
+#DOC-START define CellML solver
+#Create the problem solver CellML equations
+cellMLEquations = CMISS.CellMLEquations()
+problem.CellMLEquationsCreateStart()
+daeSolver.CellMLEquationsGet(cellMLEquations)
+cellmlIndex = cellMLEquations.CellMLAdd(cellML)
+problem.CellMLEquationsCreateFinish()
+#DOC-END define CellML solver
+
+#Create the problem solver PDE equations
+solverEquations = CMISS.SolverEquations()
+problem.SolverEquationsCreateStart()
+dynamicSolver.SolverEquationsGet(solverEquations)
+solverEquations.sparsityType = CMISS.SolverEquationsSparsityTypes.SPARSE
+equationsSetIndex = solverEquations.EquationsSetAdd(equationsSet)
+problem.SolverEquationsCreateFinish()
+
+# Prescribe any boundary conditions 
+boundaryConditions = CMISS.BoundaryConditions()
+solverEquations.BoundaryConditionsCreateStart(boundaryConditions)
+solverEquations.BoundaryConditionsCreateFinish()
+
+# Main time loop
+# Here we are really doing something that Iron is not designed to handle, so this is not optimal
+# but does allow up to test model integration in Iron.
+currentTime = 0.0
+# grab initial results
+#print "Time: " + str(currentTime)
+time = []
+if cellmlNodeThisComputationalNode:
+    time.append(currentTime)
+    sin1.append(cellMLIntermediateField.ParameterSetGetNode(CMISS.FieldVariableTypes.U, CMISS.FieldParameterSetTypes.VALUES, 1, 1, cellmlNode, sin1_Component))
+    sin2.append(cellMLStateField.ParameterSetGetNode(CMISS.FieldVariableTypes.U, CMISS.FieldParameterSetTypes.VALUES, 1, 1, cellmlNode, sin2_Component))
+    sin3.append(cellMLIntermediateField.ParameterSetGetNode(CMISS.FieldVariableTypes.U, CMISS.FieldParameterSetTypes.VALUES, 1, 1, cellmlNode, sin3_Component))
+
+while currentTime < timeStop:
+    # set the next solution interval
+    nextTime = currentTime + outputTimeStep
+    controlLoop.TimesSet(currentTime, nextTime, outputTimeStep)
+    
+    # integrate the model
+    problem.Solve()
+
+    currentTime = nextTime
+    
+    # grab results
+    #print "Time: " + str(currentTime)
+    if cellmlNodeThisComputationalNode:
+        time.append(currentTime)
+        sin1.append(cellMLIntermediateField.ParameterSetGetNode(CMISS.FieldVariableTypes.U, CMISS.FieldParameterSetTypes.VALUES, 1, 1, cellmlNode, sin1_Component))
+        sin2.append(cellMLStateField.ParameterSetGetNode(CMISS.FieldVariableTypes.U, CMISS.FieldParameterSetTypes.VALUES, 1, 1, cellmlNode, sin2_Component))
+        sin3.append(cellMLIntermediateField.ParameterSetGetNode(CMISS.FieldVariableTypes.U, CMISS.FieldParameterSetTypes.VALUES, 1, 1, cellmlNode, sin3_Component))
+    
+# save the results
+with open('results.txt', "w") as outputFile:
+    print >> outputFile, "#time Vm ICaL ICaLK ICaLNa IStim"
+    for i in range(0, len(time)):
+        print >> outputFile, time[i], sin1[i], sin2[i], sin3[i]
+         
+# Export the results, here we export them as standard exnode, exelem files
+if outputFrequency != 0:
+    fields = CMISS.Fields()
+    fields.CreateRegion(region)
+    fields.NodesExport("Monodomain","FORTRAN")
+    fields.ElementsExport("Monodomain","FORTRAN")
+    fields.Finalise()
+
