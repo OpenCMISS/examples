1e77c79e8a7de0cba1d7103c302882cfaddebc88
Benjamin Maier <maier.bn@gmail.com>
2018-01-08 10:31:27 +1300

    Adapt examples to cmake build system.

---
+++ FiniteElasticity/RightBiceps/build_debug/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /store/software/opencmiss/OpenCMISS-examples/FiniteElasticity/RightBiceps
+# Build directory: /store/software/opencmiss/OpenCMISS-examples/FiniteElasticity/RightBiceps/build_debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(Fortran)
---
+++ FiniteElasticity/RightBiceps/build_debug/Fortran/CMakeFiles/RightBiceps.dir/DependInfo.cmake
@@ -0,0 +1,42 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "Fortran"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_Fortran
+  "/store/software/opencmiss/OpenCMISS-examples/FiniteElasticity/RightBiceps/Fortran/src/FortranExample.f90" "/store/software/opencmiss/OpenCMISS-examples/FiniteElasticity/RightBiceps/build_debug/Fortran/CMakeFiles/RightBiceps.dir/src/FortranExample.f90.o"
+  )
+set(CMAKE_Fortran_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_Fortran
+  "DEBUG"
+  "DOUBLE_REAL_PRECISION"
+  "USE_CUSTOM_PROFILING"
+  "WITH_CELLML"
+  "WITH_DIAGNOSTICS"
+  "WITH_FIELDML"
+  "WITH_HYPRE"
+  "WITH_MUMPS"
+  "WITH_PETSC"
+  "WITH_SCALAPACK"
+  "WITH_SUNDIALS"
+  )
+
+# The include file search paths:
+set(CMAKE_Fortran_TARGET_INCLUDE_PATH
+  "/store/software/opencmiss/iron_maierbn/install/x86_64_linux/gnu-5.4-F5.4/openmpi_release/debug/include/opencmiss"
+  "/store/software/opencmiss/iron_maierbn/install/x86_64_linux/gnu-5.4-F5.4/openmpi_release/debug/include"
+  "/usr/lib/openmpi/include"
+  "/usr/lib/openmpi/lib"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent"
+  "/usr/lib/openmpi/include/openmpi/opal/mca/event/libevent2021/libevent/include"
+  "/usr/lib/openmpi/include/openmpi"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
